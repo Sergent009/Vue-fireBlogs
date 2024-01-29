93ea7d47ef6743b64c0ce90211add97423aea7b3
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-01-29 16:22:39 +0500

    Blog cards are added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,68 @@
 <template>
   <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCards :post="post" v-for="(post, index) in sampleBlogCards" :key="index" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost.vue'
+import BlogCards from '../components/BlogCard.vue'
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    BlogPost,
+    BlogCards
+  },
+
+  data(){
+    return{
+      welcomeScreen: {
+        title: "Welcome!",
+        blogPost: 
+        "Weekly blog articles with all things programming including HTML, CSS and JAVASCRIPT and more, Register today",
+        welcomeScreen: true,
+        photo: "coding"
+      },
+
+      sampleBlogPost: [{
+        title: "This is a filter Title!",
+        blogHTML: "this is a filter blog post title!",
+        blogCoverPhoto: "beautiful-stories"
+      },
+      {
+        title: "This is a filter Title!",
+        blogHTML: "this is a filter blog post title!",
+        blogCoverPhoto: "designed-for-everyone"
+      },
+      ],
+      
+      sampleBlogCards: [
+        {blogTitle: "Blog Card # 1", blogCoverPhoto: "stock-1", blogDate: "May 1, 2021"},
+        {blogTitle: "Blog Card # 2", blogCoverPhoto: "stock-2", blogDate: "May 1, 2021"},
+        {blogTitle: "Blog Card # 3", blogCoverPhoto: "stock-3", blogDate: "May 1, 2021"},
+        {blogTitle: "Blog Card # 4", blogCoverPhoto: "stock-4", blogDate: "May 1, 2021"},
+      ]
+    }
+  }
 };
 </script>
+
+<style scoped>
+
+h3{
+  font-weight: 300;
+  font-size: 28px;
+  margin-bottom: 32px;
+}
+
+</style>
